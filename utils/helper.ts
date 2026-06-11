7869ddc645b8ef7fd0d98e0a40bef47d02e14496
AnjanaShaji29 <AnjanaShaji29>
2026-06-11 19:33:12 +0530

    Listing With Search & Filter Added

---
+++ utils/helper.ts
@@ -0,0 +1,7 @@
+export function formatDate(dateString: string) {
+  return new Intl.DateTimeFormat("en-US", {
+    dateStyle: "medium",
+    timeStyle: "short",
+    timeZone: "UTC",
+  }).format(new Date(dateString));
+}
